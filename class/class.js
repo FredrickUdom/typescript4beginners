f61d80dd67ce9ee06a4d5acf243dce0b5744fb32
“fredrickUdom” <fredrickudom@gmail.com>
2023-06-05 14:57:44 +0100

    extending  from a class

--- class/class.js
+++ class/class.js
@@ -1,13 +1,41 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Human = /** @class */ (function () {
     function Human(n, p, i) {
         this.name = n;
-        this.pet = p;
+        this.figure = p;
         this.isCool = i;
     }
     Human.prototype.greeting = function () {
-        return "my name is: ".concat(this.name, ", and I have ").concat(this.pet, " pets, am ").concat(this.isCool);
+        return "my name is: ".concat(this.name, ", and I have ").concat(this.figure, " pets, am ").concat(this.isCool);
     };
     return Human;
 }());
 var humanBeing1 = new Human('fred', 3, true);
-console.log(humanBeing1.greeting());
+// console.log(humanBeing1.name)
+// console.log(humanBeing1.greeting())
+var People = /** @class */ (function (_super) {
+    __extends(People, _super);
+    function People(myName, age, attitude) {
+        return _super.call(this, myName, age, attitude) || this;
+    }
+    People.prototype.myDetails = function () {
+        return ("my names are: ".concat(this.name, ", I am ").concat(this.figure, " years old, my attitude:").concat(this.isCool));
+    };
+    return People;
+}(Human));
+var details = new People('Dickson', 5, true);
+console.log(details.myDetails());
--- class/class.ts
+++ class/class.ts
@@ -1,17 +1,29 @@
 class Human{
     name: string;
-    pet : number;
+    figure : number;
     isCool: boolean;
 
     constructor(n: string, p:number, i:boolean){
         this.name = n;
-        this.pet = p;
+        this.figure = p;
         this.isCool = i;
 
     }
      greeting(){
-        return `my name is: ${this.name}, and I have ${this.pet} pets, am ${this.isCool}`
+        return `my name is: ${this.name}, and I have ${this.figure} pets, am ${this.isCool}`
      }
 }
 let humanBeing1 = new Human('fred', 3, true);
-console.log(humanBeing1.greeting())
+// console.log(humanBeing1.name)
+// console.log(humanBeing1.greeting())
+
+class People extends Human{
+    constructor(myName: string, age: number, attitude: boolean){
+        super(myName, age, attitude)
+    }
+    myDetails(){
+        return(`my names are: ${this.name}, I am ${this.figure} years old, my attitude:${this.isCool}`)
+    }
+}
+let details = new People('Dickson', 5, true);
+console.log(details.myDetails())
